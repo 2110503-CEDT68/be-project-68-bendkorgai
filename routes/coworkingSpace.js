b23067a039567ca9c8a0e07e8ceaacd3b45c0281
drivepasoot <drivepasoot11@gmail.com>
2026-02-28 21:06:45 +0700

    Fix Routes

---
+++ routes/coworkingSpace.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const reservationRouter = require('./reservations');
+
+const router = express.Router();
+const {
+  getCoworkingSpaces,
+  getCoworkingSpace,
+  createCoworkingSpace,
+  updateCoworkingSpace,
+  deleteCoworkingSpace
+} = require('../controllers/coworkingSpace');
+
+const {protect, authorize} = require('../middleware/auth');
+
+router.use('/:coworkingSpaceId/reservations', reservationRouter);
+
+router.route('/').get(getCoworkingSpaces).post(protect, authorize('admin'),createCoworkingSpace);
+router.route('/:id').get(getCoworkingSpace).put(protect,authorize('admin') ,updateCoworkingSpace).delete(protect,authorize('admin') ,deleteCoworkingSpace);
+
+module.exports = router;
